0af2a438c750a8384b1cfec954be55ff9deb94d7
chaynes2019 <chaynes2019@gmail.com>
2026-03-10 20:43:51 -0400

    Add the other goal elements desired for v1

--- Goal.py
+++ Goal.py
@@ -1,10 +1,13 @@
 time_needed_adjustment_factor = 0.5
 
 class Goal():
-    def __init__(self, expected_time_to_complete, input_start_date, input_end_date):
+    def __init__(self, expected_time_to_complete, input_start_date, input_end_date, input_hours_required, input_previous_hours, input_last_weekly_hours):
         self.expected_time_needed = expected_time_to_complete
         self.start_date = input_start_date
         self.end_date = input_end_date
+        self.estimated_hours_required = input_hours_required
+        self.hours_so_far = input_previous_hours
+        self.last_week_hours_spent = input_last_weekly_hours
 
     def computeTimeDistribution(self):
         pass
